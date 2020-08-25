f6c6ac668e61238caff8daeb2d1dd06a9cc8f898
ENRIQUE ARCE ALEJANDRO <a2019112108@estudiantes.upsa.edu.bo>
2020-08-25 07:01:45 -0400

    Mi primer base de datos

---
+++ APIEstudiante/APIEstudiante/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace APIEstudiante.Models
+{
+    public class DataContext: DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<APIEstudiante.Models.Student> Students { get; set; }
+    }
+}
